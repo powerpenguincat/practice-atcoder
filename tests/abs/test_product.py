5f8053e596b80ef7069ff432b0e6924b40810ec5
ppc <ppc.develop@gmail.com>
2021-07-09 22:35:18 +0900

    Refactor test codes.

--- tests/abs/test_product.py
+++ tests/abs/test_product.py
@@ -3,10 +3,10 @@ import pytest
 from practice_atcoder.abs.product import question
 
 
-class TestReturnStr(object):
+class Test(object):
     @pytest.mark.parametrize("ab,expect", [
         ("3 4", "Even"),
         ("1 21", "Odd"),
     ])
-    def test_ok(self, ab, expect):
+    def test(self, ab, expect):
         assert question(ab) == expect
--- tests/abs/test_welcome_to_atcoder.py
+++ tests/abs/test_welcome_to_atcoder.py
@@ -3,10 +3,10 @@ import pytest
 from practice_atcoder.abs.welcome_to_atcoder import question
 
 
-class TestReturnStr(object):
+class Test(object):
     @pytest.mark.parametrize("a,bc,s,expect", [
         ("1", "2 3", "test", "6 test"),
         ("72", "128 256", "myonmyon", "456 myonmyon"),
     ])
-    def test_ok(self, a, bc, s, expect):
+    def test(self, a, bc, s, expect):
         assert question(a, bc, s) == expect
