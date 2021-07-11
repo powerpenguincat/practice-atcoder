0efa78cc678203f45a723b8efaa11108d3ad8994
ppc <ppc.develop@gmail.com>
2021-07-12 01:13:27 +0900

    Add a comment.

--- practice_atcoder/abs/traveling.py
+++ practice_atcoder/abs/traveling.py
@@ -11,10 +11,14 @@ def question(n, txyn):
 
     can = True
     for i in range(exn):
+        # 制限時間を求める
         dt = exts[i+1] - exts[i]
+        # 距離を求める
         dist = abs(exxs[i+1] - exxs[i]) + abs(exys[i+1] - exys[i])
+        # 制限時間内にたどりつけない場合
         if dt < dist:
             can = False
+        # 行ったり来たりして時間を稼げない場合
         if dist % 2 != dt % 2:
             can = False
 
